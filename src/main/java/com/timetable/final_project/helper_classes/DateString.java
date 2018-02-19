7ac8865e56b1179038dbb650d0bf66278eadeaf9
e.giannopoulos <efth90@gmail.com>
2018-02-19 16:41:58 +0100

    added date string

---
+++ src/main/java/com/timetable/final_project/helper_classes/DateString.java
@@ -0,0 +1,16 @@
+package com.timetable.final_project.helper_classes;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateString {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static LocalDate stringToLocalDate(String localDate){
+        return LocalDate.parse(localDate, formatter);
+    }
+}
